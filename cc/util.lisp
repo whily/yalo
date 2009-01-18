fb6d43ef4dd1da2092340b4ceb2721b881e7d6fd
yujian.zhang <yujian.zhang@f7454a70-184e-0410-9ab3-0bc0d8bd371b>
2009-01-18 14:52:36 +0000

    Remove awhen and acond.
    git-svn-id: https://yalo.googlecode.com/svn/trunk@50 f7454a70-184e-0410-9ab3-0bc0d8bd371b

--- cc/util.lisp
+++ cc/util.lisp
@@ -14,22 +14,6 @@
   `(let ((it ,test-form))
      (if it ,then-form ,else-form)))
 
-(defmacro awhen (test-form &body body)
-  "Anaphoric variant of when."
-  `(aif ,test-form
-        (progn ,@body)))
-
-(defmacro acond (&rest clauses)
-  "Anaphoric variant of cond."
-  (if (null clauses)
-      nil
-      (let ((cl1 (car clauses))
-            (sym (gensym)))
-        `(let ((,sym ,(car cl1)))
-           (if ,sym
-               (let ((it ,sym)) ,@(cdr cl1))
-               (acond ,@(cdr clauses)))))))
-    
 (defun repeat-element (n element)
   (loop for i from 0 below n collect element))
 
