1c7823aa0d345ed56b4ef4f3d6e4fd5d7c76ce65
yujian.zhang <yujian.zhang@f7454a70-184e-0410-9ab3-0bc0d8bd371b>
2009-01-21 15:16:26 +0000

    Add str and symb.
    git-svn-id: https://yalo.googlecode.com/svn/trunk@79 f7454a70-184e-0410-9ab3-0bc0d8bd371b

--- cc/util.lisp
+++ cc/util.lisp
@@ -10,9 +10,21 @@
 (in-package :cc)
 
 (defmacro aif (test-form then-form &optional else-form)
-  "Anaphoric variant of if."
+  "Anaphoric variant of if. From ON LISP."
   `(let ((it ,test-form))
      (if it ,then-form ,else-form)))
+
+(defun str (&rest args)
+  "Take any number of arguments and concatenates their printed
+representations into a string. FROM ON LISP."
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun symb (&rest args)
+  "Takes one or more arguments and returns the symbol whose printname
+is their concatenation."
+  (values (intern (apply #'str args))))
+  
 
 (defun repeat-element (n element)
   (loop for i from 0 below n collect element))
