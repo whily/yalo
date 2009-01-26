cedc1fce851744bdb4a6940d81e783c0285080f1
yujian.zhang <yujian.zhang@f7454a70-184e-0410-9ab3-0bc0d8bd371b>
2009-01-26 13:11:44 +0000

    Split *test-asm* into *arith-asm*, *misc-asm* and *mov-asm*.
    git-svn-id: https://yalo.googlecode.com/svn/trunk@99 f7454a70-184e-0410-9ab3-0bc0d8bd371b

--- cc/test-cc.lisp
+++ cc/test-cc.lisp
@@ -9,11 +9,10 @@
 
 (in-package :cc)
 
-(defparameter *test-asm*
+(defparameter *arith-asm*
   '((bits    16)
     (org     #x7c00)
 
-    start
     (add     al 8)
     (add     ax 1000)
     (add     bl 3)
@@ -28,10 +27,6 @@
     (add     (msg) dx)
     (add     ch (msg))
     (add     dx (msg))
-    (call    msg)
-    (clc)
-    (cld)
-    (cli)
     (cmp     al 8)
     (cmp     ax 1000)
     (cmp     bl 3)
@@ -46,6 +41,45 @@
     (cmp     (msg) dx)
     (cmp     ch (msg))
     (cmp     dx (msg))
+    (sub     al 8)
+    (sub     ax 1000)
+    (sub     bl 3)
+    (sub     byte (msg) 4)
+    (sub     bx 1234)
+    (sub     (msg) 5678)
+    (sub     cx 9)
+    (sub     word (msg) 12)
+    (sub     al bl)
+    (sub     (msg) ch)
+    (sub     cx bx)
+    (sub     (msg) dx)
+    (sub     ch (msg))
+    (sub     dx (msg))
+    (db      msg "Hello World! ")
+    endmsg)
+  "Arithmetic instructions are tested separately.")
+
+(defparameter *arith-code*
+  '(4 8 5 232 3 128 195 3 128 6 153 124 4 129 195 210 4 129 6 153 124 46 22 131
+    193 9 131 6 153 124 12 0 216 0 46 153 124 1 217 1 22 153 124 2 46 153 124 3 22
+    153 124 60 8 61 232 3 128 251 3 128 62 153 124 4 129 251 210 4 129 62 153 124
+    46 22 131 249 9 131 62 153 124 12 56 216 56 46 153 124 57 217 57 22 153 124 58
+    46 153 124 59 22 153 124 44 8 45 232 3 128 235 3 128 46 153 124 4 129 235 210
+    4 129 46 153 124 46 22 131 233 9 131 46 153 124 12 40 216 40 46 153 124 41 217
+    41 22 153 124 42 46 153 124 43 22 153 124 72 101 108 108 111 32 87 111 114 108
+    100 33 32))
+
+(defparameter *misc-asm*
+  '((bits    16)
+    (org     #x7c00)
+
+    start
+
+    (call    msg)
+    (clc)
+    (cld)
+    (cli)
+
     (hlt)
     (in      al 3)
     (in      ax 4)
@@ -57,6 +91,48 @@
     (jmp     short .loop)
     (lodsb)
     (lodsw)
+    (nop)
+    (out     3 al)
+    (out     4 ax)
+    (out     dx al)
+    (out     dx ax)
+    (push    cx)
+    (push    cs)
+    (push    ss)
+    (push    ds)
+    (push    es)
+    (pop     dx)
+    (pop     ss)
+    (pop     ds)
+    (pop     es)
+    (rep     movsb)
+    (rep     movsw)
+    (ret)
+    (stc)
+    (std)
+    (sti)
+    (stosb)
+    (stosw)
+
+    (equ     hi 4)
+    (db      msg "Hello World! ")
+    endmsg
+    (times   3 db 0)
+    (dw      #xaa55) 
+    (dd      (123456 7891011))
+    (dq      3372036854775808))
+  "Miscellaneous instructions.")
+
+(defparameter *misc-code*
+  '(232 43 0 248 252 250 244 228 3 229 4 236 237 204 205 16 235 254 172 173 144
+    230 3 231 4 238 239 81 14 22 30 6 90 23 31 7 243 164 243 165 195 249 253 251
+    170 171 72 101 108 108 111 32 87 111 114 108 100 33 32 0 0 0 85 170 64 226 1 0
+    67 104 120 0 0 0 230 130 217 250 11 0))
+
+(defparameter *mov-asm*
+  '((bits    16)
+    (org     #x7c00)
+
     (mov     ah 9)
     (mov     bx (- endmsg msg))
     (mov     ax cs)
@@ -93,50 +169,18 @@
     (mov     (msg) bx)
     (mov     cx  (#x1c7b))
     (mov     word (msg) 123)
-    (nop)
-    (out     3 al)
-    (out     4 ax)
-    (out     dx al)
-    (out     dx ax)
-    (push    cx)
-    (push    cs)
-    (push    ss)
-    (push    ds)
-    (push    es)
-    (pop     dx)
-    (pop     ss)
-    (pop     ds)
-    (pop     es)
-    (rep     movsb)
-    (rep     movsw)
-    (ret)
-    (stc)
-    (std)
-    (sti)
-    (stosb)
-    (stosw)
-    (sub     al 8)
-    (sub     ax 1000)
-    (sub     bl 3)
-    (sub     byte (msg) 4)
-    (sub     bx 1234)
-    (sub     (msg) 5678)
-    (sub     cx 9)
-    (sub     word (msg) 12)
-    (sub     al bl)
-    (sub     (msg) ch)
-    (sub     cx bx)
-    (sub     (msg) dx)
-    (sub     ch (msg))
-    (sub     dx (msg))
-    (equ     hi 4)
+    
     (db      msg "Hello World! ")
-    endmsg
-    (times   3 db 0)
-    (dw      #xaa55) 
-    (dd      (123456 7891011))
-    (dq      3372036854775808)
-    ))
+    endmsg)
+  "Mov instructions are tested separately.")
+
+(defparameter *mov-code*
+  '(180 9 187 13 0 140 200 140 24 140 1 140 26 140 19 140 28 140 13 140 30 74 126
+    140 30 105 124 140 15 140 88 1 140 65 2 140 90 3 140 83 4 140 92 5 140 77 6
+    140 94 7 140 79 8 142 195 142 29 142 152 233 3 142 129 234 3 142 154 235 3 142
+    147 236 3 142 156 237 3 142 133 238 3 142 158 239 3 142 135 240 3 137 200 137
+    30 105 124 139 14 123 28 199 6 105 124 123 0 72 101 108 108 111 32 87 111 114
+    108 100 33 32))
 
 (deftest test-cc ()
   (check 
@@ -157,20 +201,10 @@
        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 85 170))
-    (equal
-     (asm *test-asm*) 
-     '(4 8 5 232 3 128 195 3 128 6 48 125 4 129 195 210 4 129 6 48 125 46 22 131 193
-       9 131 6 48 125 12 0 216 0 46 48 125 1 217 1 22 48 125 2 46 48 125 3 22 48 125
-       232 250 0 248 252 250 60 8 61 232 3 128 251 3 128 62 48 125 4 129 251 210 4
-       129 62 48 125 46 22 131 249 9 131 62 48 125 12 56 216 56 46 48 125 57 217 57
-       22 48 125 58 46 48 125 59 22 48 125 244 228 3 229 4 236 237 204 205 16 235 254
-       172 173 180 9 187 13 0 140 200 140 24 140 1 140 26 140 19 140 28 140 13 140 30
-       74 126 140 30 48 125 140 15 140 88 1 140 65 2 140 90 3 140 83 4 140 92 5 140
-       77 6 140 94 7 140 79 8 142 195 142 29 142 152 233 3 142 129 234 3 142 154 235
-       3 142 147 236 3 142 156 237 3 142 133 238 3 142 158 239 3 142 135 240 3 137
-       200 137 30 48 125 139 14 123 28 199 6 48 125 123 0 144 230 3 231 4 238 239 81
-       14 22 30 6 90 23 31 7 243 164 243 165 195 249 253 251 170 171 44 8 45 232 3
-       128 235 3 128 46 48 125 4 129 235 210 4 129 46 48 125 46 22 131 233 9 131 46
-       48 125 12 40 216 40 46 48 125 41 217 41 22 48 125 42 46 48 125 43 22 48 125 72
-       101 108 108 111 32 87 111 114 108 100 33 32 0 0 0 85 170 64 226 1 0 67 104 120
-       0 0 0 230 130 217 250 11 0))))
+    (equal (asm *arith-asm*) *arith-code*)
+    (equal (asm *misc-asm*)  *misc-code*)
+    (equal (asm *mov-asm*)   *mov-code*)))
+
+
+
+
