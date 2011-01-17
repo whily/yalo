364bfa7204bb197b9b49efbaa8781b48d714a2a4
yujian.zhang <yujian.zhang@f7454a70-184e-0410-9ab3-0bc0d8bd371b>
2011-01-17 07:30:38 +0000

    Add copyright information.
    git-svn-id: https://yalo.googlecode.com/svn/trunk@142 f7454a70-184e-0410-9ab3-0bc0d8bd371b

--- cc/package.lisp
+++ cc/package.lisp
@@ -6,6 +6,7 @@
 ;;;; License: 
 ;;;;     GNU General Public License v2
 ;;;;     http://www.gnu.org/licenses/gpl-2.0.html
+;;;; Copyright (C) 2009-2011 Yujian Zhang
 
 (defpackage #:cc
   (:use #:common-lisp)
